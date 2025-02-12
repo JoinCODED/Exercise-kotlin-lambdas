d632747dfd8b6727ab4f7296382a11287b95f921
spdys <noorii549@yahoo.com>
2025-02-12 19:50:34 +0300

    Finished part 4 created a function called calculateDepth that takes x and y and a lambda function. Very confusing but I think I got it.

--- src/lambdaExercise.kt
+++ src/lambdaExercise.kt
@@ -1,9 +1,15 @@
 val greetSeaCreatures = { println("Hello, Deep Sea Adventurer!") }
 val findPearl: (Int) -> Int = {x -> x*x}
 val divideTreasure: (Double, Double) -> Double = {x, y -> x/y}
+val difference: (Int, Int) -> Int = { x, y -> x - y }
+
+fun calculateDepth(x: Int, y: Int, result: (Int, Int) -> Int): Int{
+    return result(x, y)
+}
 
 fun main() {
     greetSeaCreatures()
     println(findPearl(6))
     println(divideTreasure(1000.0, 5.0))
+    println(calculateDepth(1500, 800, difference))
 }
