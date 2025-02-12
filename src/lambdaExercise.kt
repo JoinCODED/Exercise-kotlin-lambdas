1e6db3596a0082a32c33ea82a813ebe7d0b8b89a
spdys <noorii549@yahoo.com>
2025-02-12 20:42:11 +0300

    Finished part 5 with the extension functions, 'loud' to make diveMessage uppercase

--- src/lambdaExercise.kt
+++ src/lambdaExercise.kt
@@ -1,10 +1,15 @@
 val greetSeaCreatures = { println("Hello, Deep Sea Adventurer!") }
 val findPearl: (Int) -> Int = {x -> x*x}
 val divideTreasure: (Double, Double) -> Double = {x, y -> x/y}
-val difference: (Int, Int) -> Int = { x, y -> x - y }
+val difference: (Int, Int) -> Int = {x, y -> x-y}
+val diveMessage: (String) -> String = { it.loud() }
 
 fun calculateDepth(x: Int, y: Int, result: (Int, Int) -> Int): Int{
     return result(x, y)
+}
+
+fun String.loud(): String {
+    return "${this.uppercase()}!"
 }
 
 fun main() {
@@ -12,4 +17,5 @@ fun main() {
     println(findPearl(6))
     println(divideTreasure(1000.0, 5.0))
     println(calculateDepth(1500, 800, difference))
+    println(diveMessage("deep sea"))
 }
