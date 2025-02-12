f2a925eb36f65a0560f86a7f7c38f94bbe005c59
wadha almarshed <wadhaalmarshed@hotmail.com>
2025-02-13 01:34:48 +0300

    all done

--- Task5/src/lambda.kt
+++ Task5/src/lambda.kt
@@ -8,10 +8,33 @@ val findPearl = {pearlNum: Int -> pearlNum * pearlNum }
 val divideTreasure = {totalTreasure: Double, explorers: Double -> totalTreasure/explorers }
 
 //Part 4: Higher-Order Function with Lambda – Calculating Depth
-//fun calculateDepth(depth_x: Int, depth_y: int): Int{
-//
-//}
+val differenceInDepth = {x: Int, y: Int -> x - y}
 
+fun calculateDepth(depth_x: Int, depth_y: Int, depthDiffCalculator: (Int, Int) -> Int): Int {
+return depthDiffCalculator(depth_x, depth_y)
+}
+
+//Part 5: Lambda with Extension Functions – Diving Message
+fun String.loud(): String {
+    return  this.uppercase() + "!"
+}
+
+val diveMessage = { message: String -> message.loud()}
+
+
+//Bonus Challenge: Lambda as an Inline Function – Exploring Uncharted Waters
+inline fun exploreWaters(action: () -> Unit){
+    val startTime = System.currentTimeMillis()
+
+    val endTime = System.currentTimeMillis()
+
+    val timeTaken = endTime - startTime
+
+    action()
+
+    println("Time taken: $timeTaken milliseconds")
+
+}
 
 
 
@@ -27,5 +50,12 @@ fun main() {
     var result: Double = divideTreasure(1000.0, 5.0)//part 3
     println(result) //part3
 
+   println(calculateDepth(1500,800, differenceInDepth))//part4
+
+    println(diveMessage("deep sea")) //part5
+
+    exploreWaters {
+        println("Exploring the unknown depths...")
+    }//Bonus
 
 }
