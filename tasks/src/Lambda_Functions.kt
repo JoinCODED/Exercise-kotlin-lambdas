ab03db575ebb5b59b67b6633221868c0dc34a56a
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-02-12 12:00:37 +0300

    Added hidden pearls calculator

--- tasks/src/Lambda_Functions.kt
+++ tasks/src/Lambda_Functions.kt
@@ -1,4 +1,6 @@
 fun main() {
     val greetSeaCreatures = {println("Hello, Deep Sea Adventurer!")}
     greetSeaCreatures()
+    val findPearl = {numPearls: Int -> numPearls * numPearls};
+    println(findPearl(6));
 }
