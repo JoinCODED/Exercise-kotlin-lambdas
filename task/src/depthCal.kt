4b12030e6f22f2d222951832f0130ac532f02b85
Sarah <sarahalenezi96@outlook.com>
2025-02-12 20:34:36 +0300

    edited depth

--- task/src/depthCal.kt
+++ task/src/depthCal.kt
@@ -1,8 +1,8 @@
-fun main(){
-    val depth = calculateDepth(1500, 800, ::diff)
+fun main() {
+    val depth = calculateDepth(1500, 800) { x, y -> x - y }
     println(depth)
 }
-fun calculateDepth(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
-    return operation(x, y)
+
+fun calculateDepth(x: Int, y: Int, differance: (Int, Int) -> Int): Int {
+    return differance(x, y)
 }
-fun diff(x: Int, y: Int): Int = x - y
