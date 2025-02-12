13ad75b51db152e9bcdf5ca030c809289b46e080
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-02-12 12:07:26 +0300

    Added treasure divider calculator

--- tasks/src/Lambda_Functions.kt
+++ tasks/src/Lambda_Functions.kt
@@ -3,4 +3,6 @@ fun main() {
     greetSeaCreatures()
     val findPearl = {numPearls: Int -> numPearls * numPearls};
     println(findPearl(6));
+    val divideTreasure = {totalTreasure: Double, numExplorers: Double -> totalTreasure / numExplorers};
+    println(divideTreasure(1000.0, 5.0));
 }
