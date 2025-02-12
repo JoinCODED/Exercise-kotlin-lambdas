2f6af3239e6ef534c0f505a4cb913d87c67ef97a
Mishal Alquraini <alqurainimishal@gmail.com>
2025-02-12 19:08:54 +0300

    part 2 done

--- task/src/Main.kt
+++ task/src/Main.kt
@@ -1,6 +1,11 @@
 fun main() {
 
     greetSeaCreatures()
+    println((findPearl(6)))
 }
 
 val greetSeaCreatures = { println("Hello, Deep Sea Adventurer!")}
+
+val findPearl: (pearl: Int) -> Int = {pearl -> pearl * pearl}
+
+val
