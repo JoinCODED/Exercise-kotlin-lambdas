4a69591a680aed9111b289c3c45f8ee8d05d1d0c
spdys <noorii549@yahoo.com>
2025-02-12 18:59:37 +0300

    Finished step 3 dividing the treasure

--- src/lambdaExercise.kt
+++ src/lambdaExercise.kt
@@ -1,7 +1,9 @@
 val greetSeaCreatures = { println("Hello, Deep Sea Adventurer!") }
 val findPearl: (Int) -> Int = {x -> x*x}
+val divideTreasure: (Double, Double) -> Double = {x, y -> x/y}
 
 fun main() {
     greetSeaCreatures()
     println(findPearl(6))
+    println(divideTreasure(1000.0, 5.0))
 }
